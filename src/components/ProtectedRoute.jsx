ce8da56b3f7e962ed6a917a1ae616b61867c5290
Nofal Ali <nofilkachhot@gmail.com>
2025-01-24 12:20:38 +0530

    Fix Success component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import DataContext from './DataContext'
 import { Navigate } from 'react-router-dom'
-import Home from '../Pages/Home'
 const ProtectedRoute = ({element}) => {
    const {cart} =  useContext(DataContext)
   return (
